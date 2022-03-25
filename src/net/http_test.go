c1669b5991038ab287370d9bd5f126722dd71454
Dell <1529146935@qq.com>
2022-03-25 22:12:35 +0800

    2022-03-25 22:00:00  UPDATED

--- src/net/http_test.go
+++ src/net/http_test.go
@@ -110,3 +110,35 @@ func TestPost(t *testing.T) {
 
 	fmt.Println(string(b))
 }
+
+func UserResp(resp http.ResponseWriter, req *http.Request) {
+	fmt.Printf("请求方法: %s \n", req.Method)
+	fmt.Printf("浏览器发送请求文件路径: %s \n", req.URL)
+	fmt.Printf("请求头: %s \n", req.Header)
+	fmt.Printf("请求包体: %s \n", req.Body)
+	fmt.Printf("客户端网络地址: %s \n", req.RemoteAddr)
+	fmt.Printf("客户端Agent: %s \n", req.UserAgent())
+
+	/**
+	  给客户端回复数据
+	*/
+	_, _ = resp.Write([]byte("User response"))
+}
+
+func IndexResp(resp http.ResponseWriter, req *http.Request) {
+	_, _ = resp.Write([]byte("Index response"))
+}
+
+/**
+
+ */
+func TestSend(t *testing.T) {
+	/*
+		为不同的请求注册不同的函数
+	*/
+	http.HandleFunc("/user", UserResp)
+	http.HandleFunc("/index", IndexResp)
+
+	//开启服务器，监听客户端的请求
+	_ = http.ListenAndServe("127.0.0.1:8888", nil)
+}
